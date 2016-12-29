aad4a0226e895191e2444edfb0eeeb5d0064c0f6
Matthew Trentacoste <web+git@matttrent.com>
2016-12-28 16:49:50 -0800

    adding forgotten tweet_id field and overriding delete

--- waterbot/models.py
+++ waterbot/models.py
@@ -1,6 +1,6 @@
 import peewee
 
-from waterbot import database
+from waterbot import database, twitter
 
 
 class BaseModel(peewee.Model):
@@ -32,4 +32,15 @@ class Tweet(BaseModel):
 	posted_at = peewee.DateTimeField()
 	reservoir = peewee.ForeignKeyField(Reservoir, related_name='tweets')
 	measure = peewee.ForeignKeyField(StorageMeasure, related_name='tweets')
+	tweet_id = peewee.BigIntegerField(null=True)
 
+	def delete_instance(**kwargs):
+		
+		if self.tweet_id is not None:
+			twapi = twitter.get_api()
+			try:
+				twapi.destroy_status(self.tweet_id)
+			except:
+				pass
+
+		super(Tweet, self).delete_instance(**kwargs)
