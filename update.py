8c70f17580487f564318f20b613ac86b0cbbecd2
Matthew Trentacoste <web+git@matttrent.com>
2016-12-29 18:50:20 -0800

    cleaning up tweet changes method

--- update.py
+++ update.py
@@ -90,61 +90,66 @@ def update_all_reservoirs():
 		update_reservoir_storage(reservoir)
 
 
-def tweet_changes():
+def tweet_changes(reservoir):
 
-	twapi = twitter.get_api()
+	last_measure = reservoir.measures.order_by(-models.StorageMeasure.date).get()
+
+	tweets = reservoir.tweets.order_by(-models.Tweet.posted_at)
+
+	last_tweet = None
+	difference = 0
+	storage_threshold = reservoir.capacity * reservoir.threshold
+
+	if tweets.count() > 0:
+		last_tweet = tweets.get()
+		difference = last_measure.storage - last_tweet.measure.storage
+
+	if last_tweet is not None and abs(difference) < storage_threshold:
+		return
+
+	direction = u'increased ⬆️'
+	if difference < 0:
+		direction = u'decreased ⬇️'
+
+	storage = last_measure.storage / 1e9
+	scale = 'billion'
+	percent = 100 * last_measure.storage / last_measure.reservoir.capacity
+
+	tweet = u'{reservoir} {direction} to {storage:0.0f} {scale} liters, {percent:2.1f}% full.'.format(
+		reservoir=reservoir.name,
+		direction=direction,
+		storage=storage,
+		scale=scale,
+		percent=percent
+	)
+
+	print tweet
+
+	tweet_id = None
+	if util.environment() == 'production':
+		twapi = twitter.get_api()
+		status = twapi.update_status(
+			tweet, lat=reservoir.latitude, long=reservoir.longitude, 
+			place_id=reservoir.twitter_place_id)
+		tweet_id = status.id
+
+	models.Tweet.create(
+		reservoir=reservoir,
+		measure=last_measure,
+		posted_at=dt.datetime.utcnow(),
+		text=tweet,
+		tweet_id=tweet_id
+	)
+
+
+def tweet_all_changes():
+
 	reservoirs = models.Reservoir.select()
-
 	for reservoir in reservoirs:
-
-		last_measure = reservoir.measures.order_by(-models.StorageMeasure.date).get()
-
-		tweets = reservoir.tweets.order_by(-models.Tweet.posted_at)
-
-		last_tweet = None
-		difference = 0
-		storage_threshold = reservoir.capacity * reservoir.threshold
-
-		if tweets.count() > 0:
-			last_tweet = tweets.get()
-			difference = last_measure.storage - last_tweet.measure.storage
-
-		if last_tweet is None or abs(difference) >= storage_threshold:
-
-			direction = u'increased ⬆️'
-			if difference < 0:
-				direction = u'decreased ⬇️'
-
-			storage = last_measure.storage / 1e9
-			scale = 'billion'
-			percent = 100 * last_measure.storage / last_measure.reservoir.capacity
-
-			tweet = u'{reservoir} {direction} to {storage:0.0f} {scale} liters, {percent:2.1f}% full.'.format(
-				reservoir=reservoir.name,
-				direction=direction,
-				storage=storage,
-				scale=scale,
-				percent=percent
-			)
-
-			print tweet
-
-			tweet_id = None
-			if util.environment() == 'production':
-				status = twapi.update_status(
-					tweet, lat=reservoir.latitude, long=reservoir.longitude, 
-					place_id=reservoir.twitter_place_id)
-				tweet_id = status.id
-
-			models.Tweet.create(
-				reservoir=reservoir,
-				measure=last_measure,
-				posted_at=dt.datetime.utcnow(),
-				text=tweet,
-				tweet_id=tweet_id
-			)
+		tweet_changes(reservoir)
 
 
 if __name__ == '__main__':
+	
 	update_all_reservoirs()
-	tweet_changes()
+	tweet_all_changes()
