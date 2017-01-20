9b17b187a8a43c242a9a8220077dbc29fa4a7a5b
Matthew Trentacoste <web+git@matttrent.com>
2017-01-19 22:25:58 -0800

    first pass at state plot script

--- scripts/state_plot.py
+++ scripts/state_plot.py
@@ -4,6 +4,7 @@
 import os
 import glob
 import json
+import requests
 
 import datetime as dt
 import numpy as np
@@ -12,7 +13,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from waterbot import config
+from waterbot import config, water_api, seasonal
 
 
 sns.set_style('white')
@@ -20,31 +21,48 @@ sns.set_style('white')
 
 if __name__ == '__main__':
     
-    today = pd.Timestamp(dt.date.today())
+    today = dt.date.today()
     first_day = today - dt.timedelta(days=60)
     last_day  = today + dt.timedelta(days=30)
 
+    reservoirs = json.load(open(config.ALL_RESERVOIR_LIST))
+
+    dfs = {}
+    for reservoir in reservoirs:
+        station_id = reservoir['station_id']
+        # print station_id,
+
+        keep_trying = True
+        while keep_trying:
+            try:
+                df = water_api.get_reservoir_storage(
+                    station_id=station_id,
+                    start_date=first_day,
+                    end_date=today
+                )
+                keep_trying = False
+            except requests.exceptions.ConnectionError:
+                pass
+                # print '.',
+        
+        dfs[station_id] = df
+        # print
+
+    dfc = seasonal.daily_state_totals(
+        dfs, first_day, today
+    )
+
     dates = pd.Series( pd.date_range(first_day, last_day, freq='D') )
 
-    # plot_df = (
-    #     df[ (df.date >= first_day) ]
-    #     .set_index('date')
-    #     .reindex(dates)
-    # )
+    plot_df = (
+        dfc
+        .set_index('date')
+        .reindex(dates)
+    )
 
-    # plot_df['day_of_year'] = pd.Series(plot_df.index, index=plot_df.index).apply(
-    #     lambda d: d.timetuple().tm_yday
-    # )
-
-    plot_df = pd.DataFrame({
-        'date': dates
-    })
-    plot_df['day_of_year'] = plot_df.date.apply(
-        lambda d: d.timetuple().tm_yday)
-    plot_df.set_index('date', inplace=True)
-
-    # plot_df['day_of_year'] = plot_df.index.apply(
-    #     lambda d: d.timetuple().tm_yday)
+    plot_df['day_of_year'] = pd.Series(plot_df.index, index=plot_df.index).apply(
+        lambda d: d.timetuple().tm_yday
+    )
 
     infile = os.path.join(
         config.SEASONAL_AVERAGE_DIR,
@@ -72,21 +90,19 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots(1, figsize=(8,8))
 
-    # min_level = .85 * min( plot_df.reservoir_storage.min(), plot_df.lo.min() )
-    # max_level = 1.1 * max( plot_df.reservoir_storage.max(), plot_df.hi.max() )
-    min_level = .85 * plot_df.lo.min()
-    max_level = 1.1 * plot_df.hi.max()
+    min_level = .85 * min( plot_df.reservoir_storage.min(), plot_df.lo.min() )
+    max_level = 1.1 * max( plot_df.reservoir_storage.max(), plot_df.hi.max() )
 
     ax.fill_between(plot_df.index, min_level, plot_df.lo, facecolor='red', alpha=0.4, linewidth=0)
     ax.fill_between(plot_df.index, plot_df.lo, plot_df.mid, facecolor='yellow', alpha=0.4, linewidth=0)
     ax.fill_between(plot_df.index, plot_df.mid, plot_df.hi, facecolor='#55cc00', alpha=0.4, linewidth=0)
     ax.fill_between(plot_df.index, plot_df.hi, max_level, facecolor='green', alpha=0.4, linewidth=0)
 
-    # plt.plot(
-    #     plot_df[ plot_df.index <= today ].index,
-    #     plot_df[ plot_df.index <= today ].reservoir_storage,
-    #     color='black', linewidth=3
-    # )
+    plt.plot(
+        plot_df[ plot_df.index <= pd.Timestamp(today) ].index,
+        plot_df[ plot_df.index <= pd.Timestamp(today) ].reservoir_storage,
+        color='black', linewidth=3
+    )
 
     # plt.plot(plot_df.loc[today].name, plot_df.loc[today].reservoir_storage, 'ko')
 
